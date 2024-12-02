231f0e23e3973a452af5b70e1f6bafa4dcc4e036
Matthew Yates <matthew.yates@sainsburys.co.uk>
2024-12-02 18:14:16 +0000

    Day02: Simplification in Main module

--- Day02/src/Main.hs
+++ Day02/src/Main.hs
@@ -9,8 +9,6 @@ main :: IO ()
 main = do
     fileContents <- readFile "input"
     let seqs = parseSeqs fileContents
-    let areSafe = map isSafeSeq seqs
-    let part1Answer = length $ filter (==True) areSafe
-    let areSafeWithRemoval = map isSafeSeqWithRemoval seqs
-    let part2Answer = length $ filter (==True) areSafeWithRemoval
+    let part1Answer = length $ filter isSafeSeq seqs
+    let part2Answer = length $ filter isSafeSeqWithRemoval seqs
     printf "Part 1 answer: %d\nPart 2 answer: %d" part1Answer part2Answer
